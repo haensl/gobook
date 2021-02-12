3e832b4a79d396fb403b2f7cd63a104bc5d897eb
HP Dietz <h.p.dietz@gmail.com>
2021-02-12 12:55:47 +0100

    Add second basename iteration.

---
+++ ch3/basename2/basename2.go
@@ -0,0 +1,23 @@
+// basename2 prints the basename(s) of the named path arguments
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	for _, arg := range os.Args[1:] {
+		fmt.Println(basename(arg))
+	}
+}
+
+func basename(s string) string {
+	slash := strings.LastIndex(s, "/")
+	s = s[slash+1:]
+	if dot := strings.LastIndex(s, "."); dot >= 0 {
+		s = s[:dot]
+	}
+	return s
+}
